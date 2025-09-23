bc9a84c6e3982017847663a1d5b87f0300605234
Jeongho Nam <samchon.github@gmail.com>
2025-09-24 04:14:20 +0900

    update generated files

--- src/MyGlobal.ts
+++ src/MyGlobal.ts
@@ -1,3 +1,4 @@
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 import dotenv from "dotenv";
@@ -5,12 +6,18 @@ import dotenvExpand from "dotenv-expand";
 import { Singleton } from "tstl";
 import typia from "typia";
 
+import { MyConfiguration } from "./MyConfiguration";
+
 /* eslint-disable */
 export class MyGlobal {
-  public static readonly prisma: PrismaClient = new PrismaClient();
   public static testing: boolean = false;
+
   public static get env(): MyGlobal.IEnvironments {
     return environments.get();
+  }
+
+  public static get prisma(): PrismaClient {
+    return prisma.get();
   }
 
   /**
@@ -64,8 +71,6 @@ export class MyGlobal {
 export namespace MyGlobal {
   export interface IEnvironments {
     API_PORT: `${number}`;
-
-    /** JWT Secret Key. */
     JWT_SECRET_KEY: string;
   }
 }
@@ -74,3 +79,12 @@ const environments = new Singleton(() => {
   dotenvExpand.expand(env);
   return typia.assert<MyGlobal.IEnvironments>(process.env);
 });
+
+const prisma = new Singleton(
+  () =>
+    new PrismaClient({
+      adapter: new PrismaBetterSQLite3({
+        url: `${MyConfiguration.ROOT}/prisma/db.sqlite`,
+      }),
+    }),
+);
--- src/setup/MySetupWizard.ts
+++ src/setup/MySetupWizard.ts
@@ -7,7 +7,7 @@ export namespace MySetupWizard {
   export async function schema(): Promise<void> {
     if (MyGlobal.testing === false)
       throw new Error(
-        "Error on SetupWizard.schema(): unable to reset database in non-test mode.",
+        "Error on MySetupWizard.schema(): unable to reset database in non-test mode.",
       );
     const execute = (type: string) => (argv: string) =>
       cp.execSync(`npx prisma migrate ${type} --schema=prisma/schema ${argv}`, {
